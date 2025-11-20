27c3cdb48acdc0af4678fb4fa84d369207a2c511
SF-bee <lianghaoran14@163.com>
2025-11-20 13:53:53 +0800

    学校练习

--- P3796.cpp
+++ P3796.cpp
@@ -26,10 +26,30 @@ struct ACautomaton{
         for(int i = 0;i < C_SIZE;i++) if(root[0].nxt[i])
             q.push(root[0].nxt[i]);
         while(!q.empty()){
-            int p = q.front();q.pop();
+            int cur = q.front();q.pop();
+            for(int i = 0;i < 26;i++){
+                if(root[cur].nxt[i]){
+                    root[root[cur].nxt[i]].fail = root[root[cur].fail].nxt[i];
+                    q.push(root[cur].nxt[i]);
+                }
+                else root[cur].nxt[i] = root[root[cur].fail].nxt[i];
+            }
         }
     }
+    int query(std::string &str){
+        int ans = 0,p = 0;
+        for(auto ch : str){
+            p = root[p].nxt[gnum(ch)];
+            for(int t = p;t && root[t].end != -1;t = root[t].fail){
+                ans += root[t].end;
+                root[t].end = -1;
+            }
+        }
+        return ans;
+    }
 };
+int n;
+std::string text;
 
 int main(){
     std::cin.tie(0) -> sync_with_stdio(0);
