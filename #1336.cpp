53ade303c51c67cbd772bc735e7744f60ec6c62f
SF-bee <lianghaoran14@163.com>
2025-11-21 13:58:50 +0800

    学校练习

--- #1336.cpp
+++ #1336.cpp
@@ -2,6 +2,7 @@
 const int MAX = 2e5 + 5;
 
 int n,m,E,T;
+bool vis[MAX];
 int p[MAX],ind[MAX],ans[MAX];
 std::vector<int> g[MAX];
 std::vector<std::pair<int,int> > e;
@@ -15,6 +16,11 @@ int main(){
         std::cin >> e[i].first >> e[i].second;
     std::cin >> T;
     for(int i = 1;i <= T;i++)
-        std::cin >> p[i];
+        std::cin >> p[i],vis[p[i]] = 1;
+    for(int i = 1;i <= E;i++) if(!p[i]){
+        g[e[i].first].push_back(e[i].second);
+        g[e[i].second].push_back(e[i].first);
+    }
+    
     return 0;
 }
