f8ada1a34c5f4776f41de2a7dce4e13eb76b0f3e
SF-bee <lianghaoran14@163.com>
2025-11-25 13:57:00 +0800

    noip加油

---
+++ #1340.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+const int MAX = 1e5 + 5;
+
+int n,m;
+int tim,top,dfn[MAX],low[MAX],ins[MAX],stk[MAX];
+int cnt,scc[MAX],siz[MAX];
+std::vector<int> g[MAX];
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    std::cin >> n >> m;
+    for(int i = 0;i < m;i++) {
+        int u,v; std::cin >> u >> v;
+        g[u].push_back(v);
+    }
+    auto dfs = [&](auto &dfs,int u) -> void {
+        low[u] = dfn[u] = ++tim, ins[u] = 1, stk[++top] = u;
+        for(auto v : g[u]) {
+            if(!dfn[v]){
+                dfs(dfs,v);
+                low[u] = std::min(low[u],dfn[v]);
+            }
+            else if(ins[v])
+                low[u] = std::min(low[u],low[v]);
+        }
+        if(low[u] == dfn[u]) {
+            cnt++;
+            do {
+                scc[stk[top]] = cnt;
+                siz[cnt]++;
+                ins[stk[top]] = 0;
+            } while(stk[top--] != u);
+        }
+    };
+    for(int i = 1;i <= n;i++)
+        if(!dfn[i]) dfs(dfs,i);
+    std::vector<int> ind(cnt + 1,0);
+    for(int u = 1;u <= n;u++)
+        for(auto v : g[u]) if(scc[u] != scc[v])
+            ind[scc[v]]++;
+    int c = 0;
+    for(int i = 1;i <= cnt;i++)
+        if(ind[i] == 0 && siz[i] == 1) c++;
+    double ans = 1.0 - (double)c / (double)n;
+    std::cout << std::fixed << std::setprecision(6) << ans << '\n';
+    return 0;
+}
--- P11362.cpp
+++ P11362.cpp
@@ -1,50 +1,82 @@
 #include <bits/stdc++.h>
 using ll = long long;
 const int MOD = 1e9 + 7;
-const int MAX = 1e5 + 5;
 
-int T,n,m;
+int T, n, m;
 ll v;
 
-ll qpow(ll x,ll c){
+ll qpow(ll x, ll c) {
     ll res = 1;
-    while (c){
-        if(c & 1) res = (res * x) % MOD;
+    while (c) {
+        if (c & 1) res = (res * x) % MOD;
         x = (x * x) % MOD;
         c >>= 1;
     }
     return res;
 }
 
-void solve(){
+void solve() {
     std::cin >> n >> m >> v;
-    std::map<int,int> auk;
-    for(int c,d,i = 1;i <= m;i++){
+    std::map<int, int> auk;
+    bool conflict = false;
+    
+    for (int i = 0; i < m; i++) {
+        int c, d;
         std::cin >> c >> d;
-        if(auk.count(c) == 0) auk[c] = d;
-        else if(auk[c] == d){
-            std::cout << 0;
-            return;
+        if (auk.count(c) && auk[c] != d) {
+            conflict = true;
         }
+        auk[c] = d;
     }
-    ll ans = qpow(v,2*((*auk.begin()).first - 1));
-    for(auto i = auk.begin();i != auk.end();i++){
-        auto p = i;
-        int l = (*p).first,r = (*(++p)).first;
-        if(p == --auk.end()) break;
-        ll cnt = qpow(v,(r - l - 1)) * (v - 1) % MOD;
-        cnt = (qpow(v,2*(r - l)) - cnt + MOD) % MOD;
-        ans = ans * cnt % MOD;
+    if (conflict) {
+        std::cout << "0\n";
+        return;
     }
-    ans = ans * qpow(v,2*(n - (*auk.rbegin()).first)) % MOD;
+    if (auk.empty()) {
+        ll ans = qpow(v * v % MOD, n - 1);
+        std::cout << ans << '\n';
+        return;
+    }
+    ll ans = 1;
+    int first_pos = auk.begin()->first;
+    if (first_pos > 1) {
+        ans = ans * qpow(v * v % MOD, first_pos - 1) % MOD;
+    }
+    
+    auto it = auk.begin();
+    auto next_it = it;
+    ++next_it;
+    
+    while (next_it != auk.end()) {
+        int l = it->first;
+        int r = next_it->first;
+        int gap = r - l - 1;
+        
+        if (gap > 0)
+            ans = ans * qpow(v * v % MOD, gap) % MOD;
+        if (it->second != next_it->second){
+            ll ways = (v * v - (v - 1) + MOD) % MOD;
+            ans = ans * ways % MOD;
+        }
+        else ans = ans * (v * v % MOD) % MOD;
+        
+        ++it;
+        ++next_it;
+    }
+    
+    int last_pos = auk.rbegin()->first;
+    if (last_pos < n) {
+        ans = ans * qpow(v * v % MOD, n - last_pos) % MOD;
+    }
+    
     std::cout << ans << '\n';
 }
 
-int main(){
-    std::cin.tie(0) -> sync_with_stdio(0);
-
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+    
     std::cin >> T;
-    while(T--)
+    while (T--)
         solve();
     return 0;
 }
