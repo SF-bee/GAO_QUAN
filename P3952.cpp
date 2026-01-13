950e97c037694e0a4fc02d873f7648c15522bbc1
SF-bee <lianghaoran14@163.com>
2026-01-13 13:31:53 +0800

    feat:P3952主体部分完成

--- P3952.cpp
+++ P3952.cpp
@@ -3,10 +3,18 @@ const int MAX = 200;
 
 int t;
 struct rep { //表示一个循环体
-    std::string i,x,y;
-    bool check() { //判断这个循环的数量级,0表示O 1,1表示O n
-        if(y != "n") return 0;
-        else return 1;
+    std::string i; //变量名
+    bool act,is_n; //act表示可以进入循环，is_n表示是否是On的循环
+    void init(std::string &num,std::string &x,std::string &y) {
+        i = num;
+        if(x == "n" && y == "n") act = true,is_n = false;
+        else if(x == "n" && y != "n") act = false,is_n = false;
+        else if(x != "n" && y == "n") act = true,is_n = true;
+        else{
+            int _x = std::stoi(x),_y = std::stoi(y);
+            act = (_x <= _y);
+            is_n = false;
+        }
     }
 };
 
@@ -24,35 +32,42 @@ void solve() {
     int tim = 0,ntim = 0; //当前复杂度数量级
     std::stack<rep> rpls;
     std::unordered_set<std::string> vis; //表示当前定义域内已经声明的变量
-    bool cannot = 0,err = 0; //表示从当前开始的循环都无法进入
+    bool inact = 0,err = 0; //inact表示是否在在有效循环中，err表示存在语法错误
     for(int i = 1;i <= l;i++){
         char op;
+        std::string num,x,y;
         rep in;
         std::cin >> op;
         switch (op){
             case 'F':
-                std::cin >> in.i >> in.x >> in.y;
-                if(err) break;
-                if(vis.find(in.i) != 0){
-                    err = 1;
-                    break;
-                }
-                if(cannot) break;
-                if(in.check()) ntim++;
-                else cannot = 1;
+                std::cin >> num >> x >> y;
+                in.init(num,x,y);
+                if(vis.count(in.i)) err = 1;
                 vis.insert(in.i);
+                if(!in.act || !inact) {
+                    inact = 0;
+                    in.act = 0;
+                }
+                else if(in.is_n) tim++;
+                ntim = std::max(ntim,tim);
                 rpls.push(in);
                 break;
             case 'E':
-                if(!rpls.top().check()) cannot = 0,ntim++;
-                rpls.pop();
-                ntim--;
+                if(rpls.empty()) err = 1;
+                else {
+                    auto [i,act,is_n] = rpls.top();
+                    rpls.pop();
+                    if(is_n) tim--;
+                    if(!act) {
+                        if(rpls.empty() || rpls.top().act)
+                            inact = true;
+                    }
+                }
                 break;
         }
-        tim = std::max(tim,ntim);
     }
     if(err || !rpls.empty()) std::cout << "ERR\n";
-    else if(tim == w) std::cout << "Yes\n";
+    else if(ntim == w) std::cout << "Yes\n";
     else std::cout << "No\n";
 }
 
