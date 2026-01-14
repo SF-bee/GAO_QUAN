c8599b18ec3c73e4fa2d7492ca6b791b8f83b5da
SF-bee <lianghaoran14@163.com>
2026-01-14 13:56:53 +0800

    AC(P5194)

--- P5194.cpp
+++ P5194.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using ll = long long;
-const int MAX = 100;
+const int MAX = 1005;
 
 int n,c;
-ll a[MAX];
+ll a[MAX],pre[MAX];
 
 int main(){
     std::cin.tie(0) -> sync_with_stdio(0);
@@ -11,14 +11,20 @@ int main(){
     std::cin >> n >> c;
     for(int i = 1;i <= n;i++){
         std::cin >> a[i];
-        if(a[i] > c) n = i - 1; 
+        if(a[i] > c){
+            n = i - 1;
+            break; 
+        }
+        pre[i] = pre[i - 1] + a[i];
     }
     
     ll ans = 0;
     auto dfs = [&](auto &self,int d,ll sum) ->void {
+        if(sum + pre[d] <= ans) return;
         ans = std::max(ans,sum);
-        if(a[d] + sum <= c)
-            self(self,d - 1,sum + a[d]);
+        for(int i = d;i;i--)
+            if(a[i] + sum <= c)
+                self(self,i - 1,sum + a[i]);
     };
     dfs(dfs,n,0);
     std::cout << ans << '\n';
