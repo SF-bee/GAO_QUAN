c880e5bd22e489b99ddd1142250390de259a4642
SF-bee <lianghaoran14@163.com>
2026-07-28 19:39:55 +0800

    AC:P6835

--- Code/Luogu/P6835.cpp
+++ Code/Luogu/P6835.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
-const int MAX = ;
+using ll = long long;
+const int MAX = 1e6 + 5;
+const ll mod = 998244353;
+
+int id, n, m;
+std::vector<int> e[MAX];
+ll f[MAX], sum[MAX];
+
+void add(ll &x, ll d) { x = (x + d) % mod; }
 
 int main() {
     std::cin.tie(0)->sync_with_stdio(0);
 
+    std::cin >> id >> n >> m;
+    for (int u, v, i = 1; i <= m; i++) {
+        std::cin >> u >> v;
+        e[u].push_back(v);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        add(f[i], e[i].size() + 1);
+        for (int v : e[i]) {
+            add(f[i], sum[i - 1] - sum[v - 1] + mod);
+        }
+        add(sum[i], sum[i - 1] + f[i]);
+    }
+    std::cout << sum[n] << '\n';
     return 0;
 }
