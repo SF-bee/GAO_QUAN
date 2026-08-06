6ce351d4c5ca8094778cdd6ddc203213658264c6
SF-bee <lianghaoran14@163.com>
2026-08-06 15:22:41 +0800

    AC:P4513

--- Code/Luogu/P4513.cpp
+++ Code/Luogu/P4513.cpp
@@ -1,78 +1,91 @@
 #include <bits/stdc++.h>
+using ll = long long;
 const int MAX = 5e5 + 5;
+const int MAXN = MAX << 2;
 
-int n,m;
-int a[MAX];
-
+int n, m;
+ll a[MAX];
 struct SegmentTree {
-    SegmentTree *ls,*rs;
-    int l,r,sum,max,maxl,maxr;
-
-    void PushUp() {
-        if(ls->maxr < 0 && rs->maxl < 0)
-            max = std::max(ls->maxr,rs->maxl);
+    struct Node {
+        int ls, rs;
+        ll sum, maxv, maxl, maxr;
+        Node() : ls(0), rs(0), sum(0), maxv(0), maxl(0), maxr(0) {};
+    } tr[MAXN];
+    int tot = 0;
+    int NewNode() {
+        tot++;
+        tr[tot].ls = tr[tot].rs = tr[tot].sum = 0;
+        return tot;
+    }
+    bool in(int l, int r, int ql, int qr) { return ql <= l && r <= qr; }
+    void PushUp(Node &p, Node &ls, Node &rs) {
+        p.sum = ls.sum + rs.sum;
+        p.maxv = std::max({ls.maxv, rs.maxv, ls.maxr + rs.maxl});
+        p.maxl = std::max(ls.maxl, ls.sum + rs.maxl);
+        p.maxr = std::max(rs.maxr, rs.sum + ls.maxr);
+    }
+    int build(int l, int r) {
+        int node = NewNode();
+        if (l == r) {
+            tr[node].maxl = tr[node].maxr = tr[node].maxv = tr[node].sum = a[l];
+            return node;
+        }
+        int mid = (l + r) >> 1;
+        tr[node].ls = build(l, mid);
+        tr[node].rs = build(mid + 1, r);
+        PushUp(tr[node], tr[tr[node].ls], tr[tr[node].rs]);
+        return node;
+    }
+    void Update(int p, int l, int r, int qp, ll k) {
+        if (l == r) {
+            tr[p].maxl = tr[p].maxr = tr[p].maxv = tr[p].sum = k;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if (qp <= mid)
+            Update(tr[p].ls, l, mid, qp, k);
+        else
+            Update(tr[p].rs, mid + 1, r, qp, k);
+        PushUp(tr[p], tr[tr[p].ls], tr[tr[p].rs]);
+        return;
+    }
+    Node Query(int p, int l, int r, int ql, int qr) {
+        if (in(l, r, ql, qr))
+            return tr[p];
+        int mid = (l + r) >> 1;
+        if (qr <= mid)
+            return Query(tr[p].ls, l, mid, ql, qr);
+        else if (ql > mid)
+            return Query(tr[p].rs, mid + 1, r, ql, qr);
         else {
-            max = 0;
-            if(ls->maxr > 0) max += ls->maxr;
-            if(rs->maxl > 0) max += rs->maxl;
-        }
-        max = std::max(ls->max,rs->max);
-        maxl = std::max(ls->maxl,ls->sum + rs->maxl);
-        maxr = std::max(rs->maxr,rs->sum + ls->maxr);
-        sum = ls->sum + rs->sum;
-    }
-    SegmentTree(int L,int R) : l(L),r(R) {
-        if(L == R){
-            ls = rs = nullptr;
-            sum = max = maxl = maxr = a[l];
-            return;
-        }
-        int mid = (L + R) >> 1;
-        ls = new SegmentTree(L,mid);
-        rs = new SegmentTree(mid + 1,R);
-        PushUp();
-    }
-    bool isIn(int L,int R) {return L <= l && r <= R;}
-    bool isOut(int pos) {return pos < l || r < pos;}
-    bool isOut(int L,int R) {return l > R || r < L;}
-    void Update(int pos,int val){
-        if(l == r && l == pos){
-            sum = max = maxl = maxr = val;
-            return;
-        }
-        else if(!isOut(pos)){
-            ls->Update(pos,val);
-            rs->Update(pos,val);
-            PushUp();
+            Node ls = Query(tr[p].ls, l, mid, ql, qr);
+            Node rs = Query(tr[p].rs, mid + 1, r, ql, qr);
+            Node res;
+            PushUp(res, ls, rs);
+            return res;
         }
     }
-    int Query(int L,int R){
-        if(isIn(L,R)) return max;
-        int res = std::max(ls->Query(L,R),rs->Query(L,R));
-        int rmax = 0;
-        if(ls->maxr > 0) rmax += ls->maxr;
-        if(rs->maxl > 0) rmax += rs->maxl;
-        return std::max(res,rmax);
-    }
-};
+} st;
 
 int main() {
-    std::cin.tie(0) -> sync_with_stdio(0);
+    std::cin.tie(0)->sync_with_stdio(0);
 
     std::cin >> n >> m;
-    for(int i = 1;i <= n;i++)
+    for (int i = 1; i <= n; i++)
         std::cin >> a[i];
-    SegmentTree *root = new SegmentTree(1,n);
-    while(m--){
-        int k,x,y;
-        std::cin >> k >> x >> y;
+    int root = st.build(1, n);
+    for (int i = 1; i <= m; i++) {
+        int k, a, b;
+        std::cin >> k >> a >> b;
         switch (k) {
         case 1:
-            if(x > y) std::swap(x,y);
-            std::cout << root->Query(x,y) << '\n';
+            if (a > b)
+                std::swap(a, b);
+            std::cout << st.Query(root, 1, n, a, b).maxv << '\n';
             break;
         case 2:
-            root->Update(x,y);
+            st.Update(root, 1, n, a, b);
+            break;
         }
     }
     return 0;
