63962f2de188305bfcc7f85f7fa9a66b5ade121d
SF-bee <lianghaoran14@163.com>
2026-08-19 21:49:40 +0800

    feat

--- Code/Luogu/T808065.cpp
+++ Code/Luogu/T808065.cpp
@@ -1,44 +1,54 @@
 #include <bits/stdc++.h>
 using ll = long long;
-const ll MAX = 5e3 + 5;
-const ll mod = 998244353;
+const int MAX = 5e3 + 5;
+const int mod = 998244353;
 
-ll n, m, k, p, q;
-// F_i 表示前 i 次成功的次数小于 k 的概率，g表示恰好 i 次成功 k - 1 次的概率
-ll inv[MAX * MAX], F, F_m, g;
-
-ll qpow(ll a, ll b) {
+int n, m, k, p, q;
+int P, fact[MAX * MAX], infact[MAX * MAX];
+int qpow(ll x, int p) {
     ll res = 1;
-    while (b) {
-        if (b & 1)
-            res = res * a % mod;
-        a = a * a % mod;
-        b >>= 1;
+    while (p) {
+        if (p & 1)
+            res = res * x % mod;
+        x = x * x % mod;
+        p >>= 1;
     }
     return res;
+}
+void init() {
+    fact[0] = 1;
+    for (int i = 1; i < MAX * MAX; i++)
+        fact[i] = 1ll * fact[i - 1] * i % mod;
+    infact[MAX * MAX - 1] = qpow(fact[MAX * MAX - 1], mod - 2);
+    for (int i = MAX * MAX - 2; i >= 0; i--)
+        infact[i] = 1ll * infact[i + 1] * (i + 1) % mod;
+}
+int C(int n, int m) {
+    if (m < 0 || m > n)
+        return 0;
+    return 1ll * fact[n] * infact[n - m] % mod * infact[m] % mod;
 }
 
 int main() {
     std::cin.tie(0)->sync_with_stdio(0);
 
+    init();
     std::cin >> n >> m >> k >> p >> q;
-
-    inv[1] = 1;
-    for (int i = 2; i <= n * m + 10; i++)
-        inv[i] = (mod - mod / i) * inv[mod % i] % mod;
-    p = p * qpow(q, mod - 2) % mod;
-    F = F_m = 1;
-    g = qpow(p, k - 1);
-    for (int i = m; i < k; i += m)
-        std::cout << 0 << ' ';
-    for (int i = k; i <= n * m; i++) {
-        F = (F - p * g + mod) % mod;
-        g = g * (i + 1) * inv[i + 2 - k] % mod * (1 - p + mod) % mod;
+    P = 1ll * p * qpow(q, mod - 2) % mod;
+    int g = qpow(P, k);
+    for (int i = 1, ans = 0; i <= n * m; i++) {
+        int res;
+        if (i < k)
+            res = 0;
+        else if (i == k)
+            res = 1ll * C(i - 1, k - 1) * g % mod;
+        else
+            g = 1ll * g * (1 - P + mod) % mod, res = 1ll * C(i - 1, k - 1) * g % mod;
+        ans = (ans + res) % mod;
         if (i % m == 0) {
-            std::cout << (F_m - F + mod) % mod << " ";
-            F_m = F;
+            std::cout << ans << ' ';
+            ans = 0;
         }
     }
-    std::cout << '\n';
     return 0;
 }
